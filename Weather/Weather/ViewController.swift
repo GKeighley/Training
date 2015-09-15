26c10bca456328a21a7c362ad401670badef396c
Glenn Keighley <gkeighley@gmail.com>
2015-09-15 16:21:19 -0700

    Checkin of weather auto-layout work

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Glenn Keighley on 9/15/15.
+//  Copyright © 2015 Epocrates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
