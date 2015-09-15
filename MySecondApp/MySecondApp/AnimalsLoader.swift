391c449a5c1ae061b02963363d72de7f228b7190
jishaq <jishaq@athenahealth.com>
2015-09-15 09:51:34 -0700

    Adding Tahoe Tessie animal

--- MySecondApp/MySecondApp/AnimalsLoader.swift
+++ MySecondApp/MySecondApp/AnimalsLoader.swift
@@ -20,6 +20,8 @@ struct AnimalsLoader {
         let bigfoot = ImaginaryAnimal(name:"Bigfoot", height:1.5, location:"Oceans", dateLastSeen:"1894",
             imageURL:NSURL(string:"https://en.wikipedia.org/wiki/Bigfoot"))
         
-        return [mermaid, bigfoot, nessie]
+        let tahoeTessie = ImaginaryAnimal(name:"Tahoe Tessie", height:4.5, location:"Lake Tahoe", dateLastSeen:"1948", imageURL:NSURL(string:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzMTNvupsi6x7kUBTTHmHKTIamX6YuvYWRPTgqEF7rz2IU-oUN"))
+        
+        return [mermaid, bigfoot, nessie, tahoeTessie]
     }
 }
