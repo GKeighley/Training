5e6bcdab8d01ddafb33bbe2cac319172e68c984b
jishaq <jishaq@athenahealth.com>
2015-09-15 09:58:20 -0700

    Fixed image URLs

--- MySecondApp/MySecondApp/AnimalsLoader.swift
+++ MySecondApp/MySecondApp/AnimalsLoader.swift
@@ -15,10 +15,10 @@ struct AnimalsLoader {
             imageURL:NSURL(string:"https://upload.wikimedia.org/wikipedia/commons/5/55/John_William_Waterhouse_A_Mermaid.jpg"))
         
         let nessie = ImaginaryAnimal(name:"Nessie", height:1.5, location:"Oceans", dateLastSeen:"1894",
-            imageURL:NSURL(string:"https://en.wikipedia.org/wiki/Nessie"))
+            imageURL:NSURL(string:"https://upload.wikimedia.org/wikipedia/en/5/5e/Hoaxed_photo_of_the_Loch_Ness_monster.jpg"))
         
         let bigfoot = ImaginaryAnimal(name:"Bigfoot", height:1.5, location:"Oceans", dateLastSeen:"1894",
-            imageURL:NSURL(string:"https://en.wikipedia.org/wiki/Bigfoot"))
+            imageURL:NSURL(string:"https://upload.wikimedia.org/wikipedia/en/9/99/Patterson%E2%80%93Gimlin_film_frame_352.jpg"))
         
         let tahoeTessie = ImaginaryAnimal(name:"Tahoe Tessie", height:4.5, location:"Lake Tahoe", dateLastSeen:"1948", imageURL:NSURL(string:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzMTNvupsi6x7kUBTTHmHKTIamX6YuvYWRPTgqEF7rz2IU-oUN"))
         
