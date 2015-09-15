86214860462bf40d22e706884854df230883ae73
Sonia Mehta <smehta@smehta-mba13.local>
2015-09-15 09:48:58 -0700

    Initial Checkin

---
+++ ImaginaryAnimalListener/ImaginaryAnimalListener/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  ImaginaryAnimalListener
+//
+//  Created by Sonia Mehta on 9/14/15.
+//  Copyright © 2015 Sonia Mehta. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var animal: ImaginaryAnimal?
+    @IBOutlet weak var lastSeenByLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var NameLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = animal?.imageURL,
+            let imageData = NSData(contentsOfURL: url) {
+                self.imageView.image = UIImage(data: imageData)
+                
+        }
+        self.NameLabel.text = animal?.name
+        self.locationLabel.text = animal?.location
+        self.heightLabel.text = animal?.height.description
+        self.lastSeenByLabel.text = animal?.dateLastSeen
+
+    }
+}
---
+++ ImaginaryAnimalListener/ImaginaryAnimalListener/ImaginaryAnimal.swift
@@ -0,0 +1,17 @@
+//
+//  ImaginaryAnimal.swift
+//  ImaginaryAnimalListener
+//
+//  Created by Sonia Mehta on 9/14/15.
+//  Copyright © 2015 Sonia Mehta. All rights reserved.
+//
+
+import Foundation
+
+struct ImaginaryAnimal {
+    var name: String
+    var height: Float
+    var location: String
+    var dateLastSeen: String
+    var imageURL: NSURL?
+}
